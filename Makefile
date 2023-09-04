3306872c2912a9ecbaa9885c4277bc35871624d5
Harshit Pandey <114309636+harshitEbPandey@users.noreply.github.com>
2023-09-04 06:23:09 +0000

    run executables

--- Makefile
+++ Makefile
@@ -7,14 +7,14 @@ all	:MAIN TEST
 MAIN	:main.o heapsort.o
 	 g++ -Wall -o MAIN main.o heapsort.o
 
-TEST	:test.o heapsort.o
-	g++ -Wall -o TEST test.o heapsort.o $(GTEST_LIB) -lpthread
+TEST	:heapsort_test.o heapsort.o
+	g++ -Wall -o TEST heapsort_test.o heapsort.o $(GTEST_LIB) -lpthread
 
 main.o	:main.cpp heapsort.h
 	 g++ -Wall -c main.cpp
 
-test.o	:test.cpp heapsort.h
-	g++ -Wall -c test.cpp
+test.o	:heapsort_test.cpp heapsort.h
+	g++ -Wall -c heapsort_test.cpp
 
 heapsort.o	:heapsort.cpp heapsort.h
 	g++ -Wall -c heapsort.cpp
--- heapsort_test.cpp
+++ heapsort_test.cpp
@@ -6,8 +6,10 @@ TEST(HeapifyTest, HeapifyWithValidInput) {
     heapify(arr, arr.size(), 0);
     
     // Add your assertions to check if the heapify function works as expected for a min-heap
+    ASSERT_EQ(arr[0], 4);
     ASSERT_EQ(arr[1], 1);
-    ASSERT_EQ(arr[2], 4);
+    ASSERT_EQ(arr[2], 9);
+    // ... Add more assertions as needed
 }
 
 TEST(HeapSortTest, HeapSortWithValidInput) {
@@ -17,6 +19,48 @@ TEST(HeapSortTest, HeapSortWithValidInput) {
     // Add your assertions to check if the heapSort function produces a sorted array in ascending order
     ASSERT_EQ(arr[0], 5);
     ASSERT_EQ(arr[1], 6);
+    ASSERT_EQ(arr[2], 7);
+    // ... Add more assertions as needed
+}
+
+TEST(HeapifyTest, InputValidation) {
+    std::vector<int> arr = {9, 5, 4, 1, 6, 3};
+    heapify(arr, arr.size(), 0);
+    
+    // Add your assertions to check if all elements in the array are integers
+    for (const auto& num : arr) {
+        ASSERT_EQ(typeid(num), typeid(int));
+    }
+}
+
+TEST(HeapifyTest, NegativeInputValidation) {
+    std::vector<int> arr = {9, 5, 4, 1, 'c', 3};
+    heapify(arr, arr.size(), 0);
+    
+    // Add your assertions to check if all elements in the array are integers
+    for (const auto& num : arr) {
+        ASSERT_EQ(typeid(num), typeid(int));
+    }
+}
+
+TEST(HeapSortTest, HeapSortWithNegativeNumbers) {
+    std::vector<int> arr = {-12, -11, -13, -5, -6, -7};
+    heapSort(arr);
+    
+    // Add your assertions to check if the heapSort function produces a sorted array with negative numbers
+    for (int i = 0; i < arr.size() - 1; i++) {
+        ASSERT_LE(arr[i], arr[i + 1]); // Check if the array is sorted in ascending order
+    }
+}
+
+TEST(HeapSortTest, HeapSortWithAllElements) {
+    std::vector<int> arr = {9, 5, 4, 1, 6, 3};
+    heapSort(arr);
+    
+    // Add your assertions to check if the heapSort function produces a sorted array with all elements
+    for (int i = 0; i < arr.size() - 1; i++) {
+        ASSERT_LE(arr[i], arr[i + 1]); // Check if the array is sorted in ascending order
+    }
 }
 
 int main(int argc, char** argv) {
